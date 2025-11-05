f1138f4487150c7e343b5889d0a906f9800795e4
Alysson Ribeiro <alyssonmelo.ti@gmail.com>
2025-11-05 00:49:50 -0300

    Explicando Blocos

---
+++ bloco.js
@@ -0,0 +1,16 @@
+// Em JavaScript, um bloco de código é definido por chaves duplas {}
+
+{
+    console.log("Este é um primeiro bloco de código em JavaScript.");
+    console.log("Embaixo colocaresmos várias sentenças em um único bloco.\n");
+
+}
+
+// Usar \n dentro de uma string cria uma nova linha ao exibir o texto no console.
+// Um bloco de código serve para agrupar múltiplas instruções em uma única unidade lógica. Ele pode ser usado em várias estruturas de controle, como funções, loops e condicionais.
+
+{
+    console.log("Este é um bloco de código em JavaScript.");
+    console.log("Ele pode conter múltiplas instruções.");
+    console.log("E todas elas serão executadas.");
+}
