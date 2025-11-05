6621044cf0f7db828de22d31136933e77e7b561e
Alysson Ribeiro <alyssonmelo.ti@gmail.com>
2025-11-05 00:26:30 -0300

    Como fazer comentários em JS

---
+++ comentário.js
@@ -0,0 +1,14 @@
+// Quando eu não quero que um código seja interpretado eu uso "//" para criar um comentário.
+
+
+console.log("Agora estamos estudando como usar // para fazer um comentário.") // Isso é um comentário após um código.
+
+// se eu colocar '//' antes de um código, ele não será interpretado.
+// console.log("Esse código não será executado.")
+
+
+console.log("Agora estamos estudando como usar /* */ para fazer um comentário em várias linhas.")
+/*
+ * Isso é um comentário de múltiplas linhas.
+ * Ele pode se estender por várias linhas.
+ */ 
