f3ae813f38d73ec96b157ba3a29d7df578e95afb
Alysson Ribeiro <alyssonmelo.ti@gmail.com>
2025-11-08 17:55:26 -0300

    Dados #01

---
+++ Início/dados_#01.js
@@ -0,0 +1,12 @@
+var nome = "Computador";
+var quantidade = 1;
+let preco = 2500.00; //a forma mais moder de criar uma variável no JS é usando let.
+let imposto = 2.5;   // mas também podemos usar var ou const.
+let precoFinal = preco + (preco * imposto / 100);
+
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
