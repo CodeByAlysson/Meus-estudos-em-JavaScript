eb0d345992918e8b4f3b661010188be19cc632a7
Alysson Ribeiro <alyssonmelo.ti@gmail.com>
2025-11-08 17:30:27 -0300

    1º exercício de bloco

---
+++ Início/desafio_sentença_bloco.js
@@ -0,0 +1,22 @@
+/*
+Colocar em um bloco os número impares de 1 a 10;
+Colocar em outro bloco os números pares de 1 a 10;
+*/
+
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+
+{
+
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+    console.log(12);
+    }
