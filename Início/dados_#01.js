456c55451045540a1b85d9a226f4bd38d94ba4ed
Alysson Ribeiro <alyssonmelo.ti@gmail.com>
2025-11-09 22:19:20 -0300

    Explicação de let e var

--- Início/dados_#01.js
+++ Início/dados_#01.js
@@ -1,6 +1,10 @@
 var nome = "Computador";
 var quantidade = 1;
-let preco = 2500.00; //a forma mais moderna de criar uma variável no JS é usando let.
+let preco = 2500.00; 
+//a forma mais moderna de criar uma variável no JS é usando let.
+// Existem duas palavras-chave para criarmos variáveis: var (antigo) ou let (moderno). 
+// Até a especificação ECMAScript 2015 (ES6), a palavra-chave var era utilizada para declarar uma variável. 
+// Entretanto, era possível declararmos duas variáveis com o mesmo nome, o que, às vezes, poderia levar a um resultado imprevisível e incorreto.
 let imposto = 2.5;   // mas também podemos usar var ou const.
 let precoFinal = preco + (preco * imposto / 100);
 
