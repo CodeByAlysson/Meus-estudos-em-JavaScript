f94580ae35f02137c3ccc0926d607368a2dbf263
Alysson Ribeiro <alyssonmelo.ti@gmail.com>
2025-11-09 22:27:06 -0300

    Explicando var, let, const e o uso de /* */ para comentários de várias linhas.

--- Início/dados_#01.js
+++ Início/dados_#01.js
@@ -3,7 +3,7 @@ var quantidade = 1;
 let preco = 2500.00; 
 /*
 
-a forma mais moderna de criar uma variável no JS é usando let.
+A forma mais moderna de criar uma variável no JS é usando let.
 Existem duas palavras-chave para criarmos variáveis: var (antigo) ou let (moderno). 
 Até a especificação ECMAScript 2015 (ES6), a palavra-chave var era utilizada para declarar uma variável. 
 Entretanto, era possível declararmos duas variáveis com o mesmo nome, o que, às vezes, poderia levar a um resultado imprevisível e incorreto.
