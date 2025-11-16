65c9b35eb4b555228cf39b8a274f7a6afe26be6c
Alysson Ribeiro <alyssonmelo.ti@gmail.com>
2025-11-15 22:33:42 -0300

    first commit

--- Início/dados_#03.js
+++ Início/dados_#03.js
@@ -1,2 +1,12 @@
 let idade = 35; 
-console.log(typeof idade); // o typeof mostra o tipo da variável
+console.log(typeof idade); // o typeof mostra o tipo da variável.
+
+let salario = 20000.90; // o JavaScript não faz distinção entre números inteiros e decimais.
+console.log(typeof salario); // salario aqui é uma variável do tipo number
+
+
+let estaCalor = true; // variáveis boolean só aceitam dois valores: true ou false
+console.log(typeof estaCalor); // aqui a variável estaCalor é do tipo boolean
+
+let nome = "Alysson Melo"; // variáveis do tipo string aceitam qualquer tipo de texto, inclusive números dentro de aspas.
+console.log(typeof nome); // aqui a variável nome é do tipo string
