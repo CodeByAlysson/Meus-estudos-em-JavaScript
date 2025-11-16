690efa419012e2a942042c9161beb0a6ed61863b
Alysson Ribeiro <alyssonmelo.ti@gmail.com>
2025-11-15 22:52:24 -0300

    first challenge

---
+++ Início/desafio_#01.js
@@ -0,0 +1,7 @@
+const PI = 3.14;
+let raio = 10; //
+let area = PI * raio * raio; // fórmula da área do círculo
+
+console.log(area); // interpolação
+console.log("O valor da área é: " + area + "m²"); // concatenação
+console.log(Math.PI); // Valor de PI mais preciso
