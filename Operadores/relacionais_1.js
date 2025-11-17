0c24f94c6174fd6ca32215250c51f4df2e2f6262
Alysson Ribeiro <alyssonmelo.ti@gmail.com>
2025-11-16 21:53:15 -0300

    Add relational operators demonstration script

---
+++ Operadores/relacionais_1.js
@@ -0,0 +1,20 @@
+// operadores relacionais - valores literais
+
+console.log(7 > 4);  // maior que
+console.log(5 < 7); // menor que
+console.log(6 >= 5);  // maior ou igual a
+console.log(20 <= 4); // menor ou igual a
+console.log(17 == 6); // igual a
+console.log(2 != 3); // diferente de
+
+// podemos ainda usar variáveis
+
+let a = 10;
+let b = 30;
+
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a == b);
+console.log(a != b);
