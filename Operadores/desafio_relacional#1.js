8b656da99001629e19476e8edea064889b6f54de
Alysson Ribeiro <alyssonmelo.ti@gmail.com>
2025-11-23 01:09:04 -0300

    Add relational operators challenge script

---
+++ Operadores/desafio_relacional#1.js
@@ -0,0 +1,16 @@
+let a = 1
+let b = 2
+
+// encontre 5 expressões que o resultado seja true
+console.log(a < b);      // true
+console.log(a <= b);     // true
+console.log(b > a);      // true
+console.log(b >= a);     // true
+console.log(a != b);     // true
+
+// encontre 5 expressões que o resultado seja false
+console.log(a > b);      // false
+console.log(a >= b);    // false
+console.log(b < a);      // false
+console.log(b <= a);     // false
+console.log(a == b);     // false  
